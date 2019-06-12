398b6c881027cd23062afb59710e50415eaeeff3
Itiwari <51078793+Itiwari@users.noreply.github.com>
2019-06-12 23:00:45 +0530

    Create Table2.sql

---
+++ Main/DDL_BUCKET/Table2.sql
@@ -0,0 +1,8 @@
+  
+spool "C:/Users/itiwari/Desktop/Table2.txt"
+CREATE TABLE Beta ( id INT PRIMARY KEY, contact INT );
+INSERT INTO Beta ( 1, 8094989700 );
+INSERT INTO Beta ( 2, 7896750090 );
+INSERT INTO Beta ( 3, 8903321456 );
+INSERT INTO Beta ( 4, 8902211134 );
+spool off;
