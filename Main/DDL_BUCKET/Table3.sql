8ea0f4c599789edfd5e722088efac672127e8578
Itiwari <51078793+Itiwari@users.noreply.github.com>
2019-06-13 14:31:17 +0530

    Create Table3.sql

---
+++ Main/DDL_BUCKET/Table3.sql
@@ -0,0 +1,7 @@
+spool "C:/Users/itiwari/Desktop/Project/Table1.txt"
+CREATE TABLE Gamma1 ( id INT PRIMARY KEY, name VARCHAR(20) );
+INSERT INTO Gamma1 VALUES ( 1, 'Alpha' );
+INSERT INTO Gamma1 VALUES ( 2, 'Beta' );
+INSERT INTO Gamma1 VALUES ( 3, 'Gamma' );
+INSERT INTO Gamma1 VALUES ( 4, 'Element' ); 
+spool off;
