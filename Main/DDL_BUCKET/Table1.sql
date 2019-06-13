9667a5737be32181895e9e4a729d4f725285c334
Itiwari <51078793+Itiwari@users.noreply.github.com>
2019-06-13 15:44:51 +0530

    Update Table1.sql

--- Main/DDL_BUCKET/Table1.sql
+++ Main/DDL_BUCKET/Table1.sql
@@ -1,6 +1,6 @@
     
 spool "C:/Users/itiwari/Desktop/Project/Table1.txt"
-CREATE TABLE Alpha2X21 ( id INT PRIMARY KEY, name VARCHAR(20) );
+CREATE TABLE AlphaX221 ( id INT PRIMARY KEY, name VARCHAR(20) );
 INSERT INTO AlphaX221 VALUES ( 1, 'Alpha' );
 INSERT INTO AlphaX221 VALUES ( 2, 'Beta' );
 INSERT INTO AlphaX221 VALUES ( 3, 'Gamma' );
