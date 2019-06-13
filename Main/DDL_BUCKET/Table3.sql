49a36c1dea5276dbe7688b9b2cad051383524d2d
Itiwari <51078793+Itiwari@users.noreply.github.com>
2019-06-13 15:03:34 +0530

    Update Table3.sql

--- Main/DDL_BUCKET/Table3.sql
+++ Main/DDL_BUCKET/Table3.sql
@@ -1,7 +1,7 @@
-spool "C:/Users/itiwari/Desktop/Project/Table1.txt"
-CREATE TABLE Gamma1 ( id INT PRIMARY KEY, name VARCHAR(20) );
-INSERT INTO Gamma1 VALUES ( 1, 'Alpha' );
-INSERT INTO Gamma1 VALUES ( 2, 'Beta' );
-INSERT INTO Gamma1 VALUES ( 3, 'Gamma' );
-INSERT INTO Gamma1 VALUES ( 4, 'Element' ); 
+spool "C:/Users/itiwari/Desktop/Project/Table3.txt"
+CREATE TABLE Gamma12 ( id INT PRIMARY KEY, name VARCHAR(20) );
+INSERT INTO Gamma12 VALUES ( 1, 'Alpha' );
+INSERT INTO Gamma12 VALUES ( 2, 'Beta' );
+INSERT INTO Gamma12 VALUES ( 3, 'Gamma' );
+INSERT INTO Gamma12 VALUES ( 4, 'Element' ); 
 spool off;
