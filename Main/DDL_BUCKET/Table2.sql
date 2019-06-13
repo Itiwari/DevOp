85a1b0c2b9ed51bf1ca287be95ac8baa5fedcaf7
Itiwari <51078793+Itiwari@users.noreply.github.com>
2019-06-13 15:04:31 +0530

    Update Table2.sql

--- Main/DDL_BUCKET/Table2.sql
+++ Main/DDL_BUCKET/Table2.sql
@@ -1,8 +1,8 @@
   
-spool "C:/Users/itiwari/Desktop/Table2.txt"
-CREATE TABLE Beta1 ( id INT PRIMARY KEY, contact INT );
-INSERT INTO Beta1 ( 1, 8094989700 );
-INSERT INTO Beta1 ( 2, 7896750090 );
-INSERT INTO Beta1 ( 3, 8903321456 );
-INSERT INTO Beta1 ( 4, 8902211134 );
+spool "C:/Users/itiwari/Desktop/Project/Table2.txt
+CREATE TABLE Beta12 ( id INT PRIMARY KEY, contact INT );
+INSERT INTO Beta12 ( 1, 8094989700 );
+INSERT INTO Beta12 ( 2, 7896750090 );
+INSERT INTO Beta12 ( 3, 8903321456 );
+INSERT INTO Beta12 ( 4, 8902211134 );
 spool off;
