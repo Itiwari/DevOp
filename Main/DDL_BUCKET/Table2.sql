7b0fe2cc5818dcba24b21960f682f1825e1aaf20
Itiwari <51078793+Itiwari@users.noreply.github.com>
2019-06-13 16:12:49 +0530

    Update Table2.sql

--- Main/DDL_BUCKET/Table2.sql
+++ Main/DDL_BUCKET/Table2.sql
@@ -1,8 +1,8 @@
   
 spool "C:/Users/itiwari/Desktop/Project/Table2.txt"
-CREATE TABLE AlphaXX221 ( id INT PRIMARY KEY, name VARCHAR(20) );
-INSERT INTO AlphaXX221 VALUES ( 1, 'Alpha' );
-INSERT INTO AlphaXX221 VALUES ( 2, 'Beta' );
-INSERT INTO AlphaXX221 VALUES ( 3, 'Gamma' );
-INSERT INTO AlphaXX221 VALUES ( 4, 'Element' ); 
+CREATE TABLE AlphaXX1221 ( id INT PRIMARY KEY, name VARCHAR(20) );
+INSERT INTO AlphaXX1221 VALUES ( 1, 'Alpha' );
+INSERT INTO AlphaXX1221 VALUES ( 2, 'Beta' );
+INSERT INTO AlphaXX1221 VALUES ( 3, 'Gamma' );
+INSERT INTO AlphaXX1221 VALUES ( 4, 'Element' ); 
 spool off;
