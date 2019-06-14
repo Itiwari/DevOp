65666ab94c200e711c594d4966360207c9fb5b71
Itiwari <51078793+Itiwari@users.noreply.github.com>
2019-06-14 14:30:07 +0530

    Update Table1.sql

--- Main/DDL_BUCKET/Table1.sql
+++ Main/DDL_BUCKET/Table1.sql
@@ -1,8 +1,8 @@
     
 spool "C:/Users/itiwari/Desktop/Project/Table1.txt"
-CREATE TABLE Alpho ( id INT PRIMARY KEY, name VARCHAR(20) );
-INSERT INTO Alpho VALUES ( 1, 'Alpha' );
-INSERT INTO Alpho VALUES ( 2, 'Beta' );
-INSERT INTO Alpho VALUES ( 3, 'Gamma' );
-INSERT INTO Alpho VALUES ( 4, 'Element' ); 
+CREATE TABLE Alpho1 ( id INT PRIMARY KEY, name VARCHAR(20) );
+INSERT INTO Alpho1 VALUES ( 1, 'Alpha' );
+INSERT INTO Alpho1 VALUES ( 2, 'Beta' );
+INSERT INTO Alpho1 VALUES ( 3, 'Gamma' );
+INSERT INTO Alpho1 VALUES ( 4, 'Element' ); 
 spool off;
