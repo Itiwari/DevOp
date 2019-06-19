890ee479c38e4c073445ca48524eca8bca9f72c1
Itiwari <51078793+Itiwari@users.noreply.github.com>
2019-06-19 14:18:02 +0530

    Update Table2.sql

--- Main/DDL_BUCKET/Table2.sql
+++ Main/DDL_BUCKET/Table2.sql
@@ -1,8 +1,10 @@
   
 spool "C:/Users/itiwari/Desktop/Project/Table2.txt"
-CREATE TABLE Alphi21 ( id INT PRIMARY KEY, name VARCHAR(20) );
-INSERT INTO Alphi21 VALUES ( 1, 'Alpha' );
-INSERT INTO Alphi21 VALUES ( 2, 'Beta' );
-INSERT INTO Alphi21 VALUES ( 3, 'Gamma' );
-INSERT INTO Alphi21 VALUES ( 4, 'Element' ); 
+CREATE TABLE Beta25 ( id INT PRIMARY KEY, name VARCHAR(20) );
+INSERT INTO Beta25 VALUES ( 1, 'Alpha' );
+INSERT INTO Beta25 VALUES ( 2, 'Beta' );
+INSERT INTO Beta25 VALUES ( 3, 'Gamma' );
+INSERT INTO Beta25 VALUES ( 4, 'Delta' ); 
+INSERT INTO Beta25 VALUES ( 5, 'Einstein' ); 
+INSERT INTO Beta25 VALUES ( 6, 'Faraday' ); 
 spool off;
