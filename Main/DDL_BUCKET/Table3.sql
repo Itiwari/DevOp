64afc864a8a1958e9edb9b8b9f975b5f427eb65f
Itiwari <51078793+Itiwari@users.noreply.github.com>
2019-06-19 14:25:32 +0530

    Update Table3.sql

--- Main/DDL_BUCKET/Table3.sql
+++ Main/DDL_BUCKET/Table3.sql
@@ -1,11 +1,11 @@
 spool "C:/Users/itiwari/Desktop/Project/Table3.txt"
-CREATE TABLE Gamma25 ( id INT PRIMARY KEY, name VARCHAR(20) );
-INSERT INTO Gamma25 VALUES ( 1, 'Alpha' );
-INSERT INTO Gamma25 VALUES ( 2, 'Beta' );
-INSERT INTO Gamma25 VALUES ( 3, 'Gamma' );
-INSERT INTO Gamma25 VALUES ( 4, 'Delta' ); 
-INSERT INTO Gamma25 VALUES ( 5, 'Einstein' );
-INSERT INTO Gamma25 VALUES ( 6, 'Faraday' );
-INSERT INTO Gamma25 VALUES ( 7, 'Galileo' );
-INSERT INTO Gamma25 VALUES ( 8, 'Harrington' );
+CREATE TABLE Gamma025 ( id INT PRIMARY KEY, name VARCHAR(20) );
+INSERT INTO Gamma025 VALUES ( 1, 'Alpha' );
+INSERT INTO Gamma025 VALUES ( 2, 'Beta' );
+INSERT INTO Gamma025 VALUES ( 3, 'Gamma' );
+INSERT INTO Gamma025 VALUES ( 4, 'Delta' ); 
+INSERT INTO Gamma025 VALUES ( 5, 'Einstein' );
+INSERT INTO Gamma025 VALUES ( 6, 'Faraday' );
+INSERT INTO Gamma025 VALUES ( 7, 'Galileo' );
+INSERT INTO Gamma025 VALUES ( 8, 'Harrington' );
 spool off;
