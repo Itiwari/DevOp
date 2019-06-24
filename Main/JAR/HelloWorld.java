f10157d8a21e1f21a8577b70847cc3f64dbef947
Itiwari <51078793+Itiwari@users.noreply.github.com>
2019-06-24 15:58:23 +0530

    Update HelloWorld.java

--- Main/JAR/HelloWorld.java
+++ Main/JAR/HelloWorld.java
@@ -2,7 +2,6 @@
 public class HelloWorld {
 public static void main(String[] args)
 {
-	System.out.println("Jar files execution started");
-	System.out.println("Hello Jenkins from Jar files");
+	System.out.println("Jar files execution started, Hello Jenkins from Jar files");
 }
 }
