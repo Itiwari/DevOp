193b8392dc7ad525dce2b518bbe8c57ce9614edf
Itiwari <51078793+Itiwari@users.noreply.github.com>
2019-06-27 12:43:42 +0530

    Add files via upload

---
+++ Main/JAR/HelloJenkins.java
@@ -0,0 +1,7 @@
+package com.air.maruti.VariousJavaUtils;
+
+public class HelloJenkins {
+  public static void main(String[] args) {
+    System.out.println("What's up Jenkins?");
+  }
+}
---
+++ Main/JAR/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.air.maruti.VariousJavaUtils;
+public class HelloWorld {
+public static void main(String[] args)
+{
+	System.out.println("Jar files execution started, Hello Jenkins from Jar files");
+}
+}
