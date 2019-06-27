c7bf8d44bcb6e1970cd7dc0ce86b3b24d03a8c54
Itiwari <51078793+Itiwari@users.noreply.github.com>
2019-06-27 12:41:01 +0530

    Delete HelloWorld.java

--- Main/JAR/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package com.air.maruti.VariousJavavUtils;
-public class HelloWorld {
-public static void main(String[] args)
-{
-	System.out.println("Jar files execution started, Hello Jenkins from Jar files");
-}
-}
