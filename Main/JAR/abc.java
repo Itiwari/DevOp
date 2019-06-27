e0c50ff90bde488c216f0c1b8371cc6e0e6c83fc
Itiwari <51078793+Itiwari@users.noreply.github.com>
2019-06-27 12:38:56 +0530

    Update abc.java

--- Main/JAR/abc.java
+++ Main/JAR/abc.java
@@ -1,8 +1,8 @@
-package classes;
+package com.air.maruti.VariousJavaUtils;
 
 public class abc {
   public static void main(String[] args) {
-    System.out.println("What's up Jnekins?");
+    System.out.println("What's up Jenkins?");
   }
 }
 
