fc26c4dd822500b01cb72f9ad9ab1a52a9c96a04
Itiwari <51078793+Itiwari@users.noreply.github.com>
2019-06-30 17:56:11 +0530

    Update Customers_Show_Duplicate.sql

--- Main/DML_BUCKET/Customers_Show_Duplicate.sql
+++ Main/DML_BUCKET/Customers_Show_Duplicate.sql
@@ -1,4 +1,4 @@
-spool "C:/Users/itiwari/Desktop/Project/Customers_Show_Duplicate";
+spool "C:/Users/itiwari/Desktop/Project/Customers_Show_Duplicate.txt";
 set serveroutput on;
 SELECT * FROM Customers;
 spool off;
