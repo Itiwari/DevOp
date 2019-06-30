4266744eb31cc6f0762066ad6ba7a61394d33078
Itiwari <51078793+Itiwari@users.noreply.github.com>
2019-06-30 19:52:36 +0530

    Update Table2.sql

--- Main/DDL_BUCKET/Table2.sql
+++ Main/DDL_BUCKET/Table2.sql
@@ -1,10 +1,10 @@
   
 spool "C:/Users/itiwari/Desktop/Project/Table2.txt"
-CREATE TABLE Beta0250 ( id INT PRIMARY KEY, name VARCHAR(20) );
-INSERT INTO Beta0250 VALUES ( 1, 'Alpha' );
-INSERT INTO Beta0250 VALUES ( 2, 'Beta' );
-INSERT INTO Beta0250 VALUES ( 3, 'Gamma' );
-INSERT INTO Beta0250 VALUES ( 4, 'Delta' ); 
-INSERT INTO Beta0250 VALUES ( 5, 'Einstein' ); 
-INSERT INTO Beta0250 VALUES ( 6, 'Faraday' ); 
+CREATE TABLE qwerty2 ( id INT PRIMARY KEY, name VARCHAR(20) );
+INSERT INTO qwerty2 VALUES ( 1, 'Alpha' );
+INSERT INTO qwerty2 VALUES ( 2, 'Beta' );
+INSERT INTO qwerty2 VALUES ( 3, 'Gamma' );
+INSERT INTO qwerty2 VALUES ( 4, 'Delta' ); 
+INSERT INTO qwerty2 VALUES ( 5, 'Einstein' ); 
+INSERT INTO qwerty2 VALUES ( 6, 'Faraday' ); 
 spool off;
