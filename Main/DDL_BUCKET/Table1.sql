c72db2f01ef6f370e17d1fd3360496f0570e7946
Itiwari <51078793+Itiwari@users.noreply.github.com>
2019-07-02 16:25:45 +0530

    Update Table1.sql

--- Main/DDL_BUCKET/Table1.sql
+++ Main/DDL_BUCKET/Table1.sql
@@ -1,9 +1,9 @@
     
 spool "C:/Users/itiwari/Desktop/Project/Table1.txt"
-CREATE TABLE qwerty250 ( id INT PRIMARY KEY, name VARCHAR(20) );
-INSERT INTO qwerty250 VALUES ( 1, 'Alpha' );
-INSERT INTO qwerty250 VALUES ( 2, 'Beta' );
-INSERT INTO qwerty250 VALUES ( 3, 'Gamma' );
-INSERT INTO qwerty250 VALUES ( 4, 'Delta' ); 
-INSERT INTO qwerty250 VALUES ( 5, 'Elphinstone' ); 
+CREATE TABLE qwert250 ( id INT PRIMARY KEY, name VARCHAR(20) );
+INSERT INTO qwert250 VALUES ( 1, 'Alpha' );
+INSERT INTO qwert250 VALUES ( 2, 'Beta' );
+INSERT INTO qwert250 VALUES ( 3, 'Gamma' );
+INSERT INTO qwert250 VALUES ( 4, 'Delta' ); 
+INSERT INTO qwert250 VALUES ( 5, 'Elphinstone' ); 
 spool off;
