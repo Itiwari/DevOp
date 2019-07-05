8c2a576f6f7e776f25e19c5c7932452821e0a3cb
Itiwari <51078793+Itiwari@users.noreply.github.com>
2019-07-05 16:37:37 +0530

    Update Table1.sql

--- Main/DDL_BUCKET/Table1.sql
+++ Main/DDL_BUCKET/Table1.sql
@@ -1,10 +1,10 @@
     
 spool "C:/Users/itiwari/Desktop/Project/Table1.txt"
-CREATE TABLE cuwq212 ( id INT PRIMARY KEY, name VARCHAR(20) );
-INSERT INTO cuwq212 VALUES ( 1, 'Alpha' );
-INSERT INTO cwuq212 VALUES ( 2, 'Beta' );
-INSERT INTO cwuq212 VALUES ( 3, 'Gamma' );
-INSERT INTO cwuq212 VALUES ( 4, 'Delta' ); 
-INSERT INTO cwuq212 VALUES ( 5, 'Elphinstone' ); 
-INSERT INTO cwuq212 VALUES ( 6, 'Fartsy' ); 
+CREATE TABLE cwuq211 ( id INT PRIMARY KEY, name VARCHAR(20) );
+INSERT INTO cwuq211 VALUES ( 1, 'Alpha' );
+INSERT INTO cwuq211 VALUES ( 2, 'Beta' );
+INSERT INTO cwuq211 VALUES ( 3, 'Gamma' );
+INSERT INTO cwuq211 VALUES ( 4, 'Delta' ); 
+INSERT INTO cwuq211 VALUES ( 5, 'Elphinstone' ); 
+INSERT INTO cwuq211 VALUES ( 6, 'Fartsy' ); 
 spool off;
