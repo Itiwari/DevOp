bdcc27581fc7e54f4273506a2a1ddae464596194
Itiwari <51078793+Itiwari@users.noreply.github.com>
2019-07-09 11:16:01 +0530

    Update Table1.sql

--- Main/DDL_BUCKET/Table1.sql
+++ Main/DDL_BUCKET/Table1.sql
@@ -1,10 +1,7 @@
     
 spool "C:/Users/itiwari/Desktop/Project/Table1.txt"
-CREATE TABLE Rqwe ( id INT PRIMARY KEY, name VARCHAR(20) );
-INSERT INTO Rqwe VALUES ( 1, 'Alpha' );
-INSERT INTO Rqwe VALUES ( 2, 'Beta' );
-INSERT INTO Rqwe VALUES ( 3, 'Gamma' );
-INSERT INTO Rqwe VALUES ( 4, 'Delta' ); 
-INSERT INTO Rqwe VALUES ( 5, 'Elphinstone' ); 
-INSERT INTO Rqwe VALUES ( 6, 'Fartsy' ); 
+CREATE TABLE PACK ( id INT PRIMARY KEY, name VARCHAR(20) );
+INSERT INTO PACK VALUES ( 1, 'Alpha' );
+INSERT INTO PACK VALUES ( 2, 'Beta' );
+INSERT INTO PACK VALUES ( 3, 'Gamma' );
 spool off;
