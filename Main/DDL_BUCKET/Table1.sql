80571c34430e20e9984cc8a81525b3e2fe680429
Itiwari <51078793+Itiwari@users.noreply.github.com>
2019-07-10 12:32:21 +0530

    Update Table1.sql

--- Main/DDL_BUCKET/Table1.sql
+++ Main/DDL_BUCKET/Table1.sql
@@ -1,7 +1,7 @@
     
 spool "C:/Users/itiwari/Desktop/Project/Table1.txt"
-CREATE TABLE PACK2 ( id INT PRIMARY KEY, name VARCHAR(20) );
-INSERT INTO PACK2 VALUES ( 1, 'Alpha' );
-INSERT INTO PACK2 VALUES ( 2, 'Beta' );
-INSERT INTO PACK2 VALUES ( 3, 'Gamma' );
+CREATE TABLE PACK3 ( id INT PRIMARY KEY, name VARCHAR(20) );
+INSERT INTO PACK3 VALUES ( 1, 'Alpha' );
+INSERT INTO PACK3 VALUES ( 2, 'Beta' );
+INSERT INTO PACK3 VALUES ( 3, 'Gamma' );
 spool off;
