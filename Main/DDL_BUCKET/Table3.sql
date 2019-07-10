bfe345e29c028414cdc5c19026ac2c10510cb2d6
Itiwari <51078793+Itiwari@users.noreply.github.com>
2019-07-10 13:05:42 +0530

    Update Table3.sql

--- Main/DDL_BUCKET/Table3.sql
+++ Main/DDL_BUCKET/Table3.sql
@@ -1,11 +1,11 @@
 spool "C:/Users/itiwari/Desktop/Project/Table3.txt"
-CREATE TABLE qwerty3 ( id INT PRIMARY KEY, name VARCHAR(20) );
-INSERT INTO qwerty3 VALUES ( 1, 'Alpha' );
-INSERT INTO qwerty3 VALUES ( 2, 'Beta' );
-INSERT INTO qwerty3 VALUES ( 3, 'Gamma' );
-INSERT INTO qwerty3 VALUES ( 4, 'Delta' ); 
-INSERT INTO qwerty3 VALUES ( 5, 'Einstein' );
-INSERT INTO qwerty3 VALUES ( 6, 'Faraday' );
-INSERT INTO qwerty3 VALUES ( 7, 'Galileo' );
-INSERT INTO qwerty3 VALUES ( 8, 'Harrington' );
+CREATE TABLE qwerty34 ( id INT PRIMARY KEY, name VARCHAR(20) );
+INSERT INTO qwerty34 VALUES ( 1, 'Alpha' );
+INSERT INTO qwerty34 VALUES ( 2, 'Beta' );
+INSERT INTO qwerty34 VALUES ( 3, 'Gamma' );
+INSERT INTO qwerty34 VALUES ( 4, 'Delta' ); 
+INSERT INTO qwerty34 VALUES ( 5, 'Einstein' );
+INSERT INTO qwerty34 VALUES ( 6, 'Faraday' );
+INSERT INTO qwerty34 VALUES ( 7, 'Galileo' );
+INSERT INTO qwerty34 VALUES ( 8, 'Harrington' );
 spool off;
